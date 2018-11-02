57d1b3d3e47cc053887e2126b79d90ef92116319
Keith Peterson <keithpe@gmail.com>
2018-11-01 20:53:14 -0400

    Add support for using my words api to return a word (words.sumarap.com)

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,15 +1,22 @@
 /////////////////////////////////////////////////////////////////////////////
 // API location and usage:
-// http://puzzle.mead.io/puzzle?wordCount=2
+// https://puzzle.mead.io/puzzle?wordCount=2
+// puzzle.mead.io returns 'puzzle'
+// return data.puzzle
+//
+// https://words.sumarap.com
+// words.sumarap.com returns 'word'
+// return data.word
 /////////////////////////////////////////////////////////////////////////////
 const getPuzzle = async (wordCount = 2) => {
   const response = await fetch(
-    `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`,
+    //`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`,
+    `https://words.sumarap.com`,
     {},
   )
   if (response.status === 200) {
     const data = await response.json()
-    return data.puzzle
+    return data.word
   } else {
     throw new Error('Unable to fetch puzzle')
   }
