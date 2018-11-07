ef53630b04ea2829a2c105786cb296f36e835782
Keith Peterson <keithpe@gmail.com>
2018-11-07 15:47:47 -0500

    Add support for updating cloudfront distribution with node js script
    Create a nodejs script to:
    1) update app files on s3,
    2) Get the current cloudfront distribution configuration
    3) Modify the cloudfront distribution configuration (ready it for updating the distribution)
    4) Update the cloudfront distribution with new configuration

---
+++ update-site.js
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////
+// Push the hangman app to AWS s3,
+// get the existing cloudfront distribution config
+// modify the config file and
+// Update the cloudfront distribution
+/////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// 1) DONE: Create a timeStamp variable to use for the subfolder name to copy
+//    hangman files.
+// 2) DONE: Copy files to s3
+// 3) DONE: Pull current cloudfront JSON configuration file
+// 4) DONE: Modify config file
+//    a) Change OriginPath
+//    b) Remove ETag (save it to a variable first)
+//    c) Remove Distribution
+//    d) Add IfMatch (original ETag, saved above)
+//  5) Update Cloudfront distribution with new configuration
+////////////////////////////////////////////////////////////////////////////////
+
+// const fs = require('fs')
+const moment = require('moment')
+const {
+  pushFilesToS3Bucket,
+  fixCloudFrontDistribution,
+  updateCloudFrontDistribution,
+  outputToFile,
+} = require('./utils')
+
+const CONFIG_FILE = 'update.json'
+console.log('configFile', CONFIG_FILE)
+
+const NEW_ORIGIN_PATH = '/' + moment().format('YYYYMMDDHHmm')
+console.log('timeStamp', NEW_ORIGIN_PATH)
+
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3()
+const CLOUD_FRONT = new AWS.CloudFront()
+const BUCKET = `hangman.sumarap.com${NEW_ORIGIN_PATH}`
+
+// Files to copy to s3
+const files = [
+  'index.html',
+  'pen.png',
+  'scripts/app.js',
+  'scripts/functions.js',
+  'scripts/hangman.js',
+  'scripts/requests.js',
+  'styles/styles_1.css',
+]
+// Copy app files to s3
+files.forEach(file => {
+  pushFilesToS3Bucket(S3, BUCKET, file)
+})
+
+// We need the cloudfront distribution id to call getDistribution
+let getDistParams = {
+  Id: 'E1NP1XD3IG47GS',
+}
+
+// Get the current cloudfront distribution
+CLOUD_FRONT.getDistribution(getDistParams, function(err, data) {
+  if (err) console.log(err, err.stack)
+  else {
+    // TODO: Remove this after testing
+    // Output original distribution config to a file.
+    //const fileOriginal = './original.json'
+    //outputToFile(fileOriginal, JSON.stringify(data, null, ' '))
+
+    const ETAG = data.ETag // Save for later
+    fixCloudFrontDistribution(data, NEW_ORIGIN_PATH, ETAG)
+
+    CLOUD_FRONT.updateDistribution(data, function(err, data) {
+      if (err) console.log(err, err.stack)
+      else {
+        console.log('updateDistribution was successful')
+      }
+    })
+
+    // TODO: Remove this after testing
+    // Outout modified distribution config to a file.
+    //const fileModified = './data.json'
+    //outputToFile(fileModified, JSON.stringify(data, null, ' '))
+  }
+})
+
+return
---
+++ utils.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+const pushFilesToS3Bucket = (S3, BUCKET, file) => {
+  let params = {
+    Bucket: BUCKET,
+    Key: file,
+    StorageClass: 'STANDARD_IA',
+  }
+
+  S3.putObject(params, function(err, data) {
+    if (err) console.log(err, err.stack)
+    else console.log(data)
+  })
+}
+
+// Change the value of OriginPath
+const fixCloudFrontDistribution = (data, NEW_ORIGIN_PATH, ETAG) => {
+  data.DistributionConfig.Origins.Items[0].OriginPath = NEW_ORIGIN_PATH
+  delete data.ETag
+  delete data.Distribution
+  data.IfMatch = ETAG
+}
+
+const outputToFile = (path, buffer) => {
+  fs.open(path, 'w', function(err, fd) {
+    if (err) {
+      throw 'could not open file: ' + err
+    }
+
+    // write the contents of the buffer, from position 0 to the end, to the file descriptor returned in opening our file
+    fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+      if (err) throw 'error writing file: ' + err
+      fs.close(fd, function() {
+        console.log('wrote the file successfully')
+      })
+    })
+  })
+}
+
+module.exports = {
+  pushFilesToS3Bucket: pushFilesToS3Bucket,
+  fixCloudFrontDistribution: fixCloudFrontDistribution,
+  outputToFile,
+}
